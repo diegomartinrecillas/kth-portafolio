eafdeb17f033b1484925ec356f190e881b750c92
Diego Martin <diegomartinrecillas@outlook.com>
2020-04-24 11:29:22 +0200

    added projects

--- src/portafolio.ts
+++ src/portafolio.ts
@@ -6,18 +6,22 @@ export interface Project {
 export const PROJECTS: Project[] = [
   {
     imageDescription: "CERO Challenge redisgn",
-    imageUri: "media/CERO Challenge Redesign.svg"
+    imageUri: "media/cero.svg",
   },
   {
     imageDescription: "Rundbok",
-    imageUri: "media/rundbok.png"
+    imageUri: "media/rundbok.png",
   },
   {
-    imageDescription: "Rundbok",
-    imageUri: "media/rundbok.png"
+    imageDescription: "Rundbok Mobile",
+    imageUri: "media/rundbok-mobile.svg",
   },
   {
     imageDescription: "World Debt Visualization",
-    imageUri: "media/world-debt.png"
-  }
+    imageUri: "media/world-debt.png",
+  },
+  {
+    imageDescription: "Wordxplore AR",
+    imageUri: "media/wordxplore.png",
+  },
 ];
